7f6f7ec1e00f8da8dcb135cb55a849783627a245
Arts Beacon <jedost@gmail.com>
2020-05-12 01:38:18 -0400

    styles

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -11,64 +11,16 @@ import '@reach/skip-nav/styles.css'
 import Footer from './Footer'
 import SEO from './SEO'
 import SkipNavLink from './SkipNavLink'
-import { theme, reset } from '../styles'
+import { theme, reset, artsbeacon } from '../styles'
 
-import 'typeface-lora'
-import 'typeface-source-sans-pro'
+// import 'typeface-lora'
+// import 'typeface-source-sans-pro'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const globalStyle = css`
   ${reset}
-  h1, h2, h3, h4, h5, h6 {
-    color: ${theme.colors.black};
-  }
-  html {
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-  body {
-    color: ${theme.colors.greyDarker};
-    background-color: ${theme.colors.bg};
-  }
-  ::selection {
-    color: ${theme.colors.bg};
-    background-color: ${theme.colors.primary};
-  }
-  a {
-    color: ${theme.colors.primary};
-    transition: all 0.4s ease-in-out;
-    text-decoration: none;
-    font-weight: 700;
-    font-style: italic;
-    &:hover,
-    &:focus {
-      text-decoration: underline;
-    }
-  }
-  @media (max-width: ${theme.breakpoints.m}) {
-    html {
-      font-size: 16px !important;
-    }
-  }
-  @media (max-width: ${theme.breakpoints.s}) {
-    h1 {
-      font-size: 2.369rem !important;
-    }
-    h2 {
-      font-size: 1.777rem !important;
-    }
-    h3 {
-      font-size: 1.333rem !important;
-    }
-    h4 {
-      font-size: 1rem !important;
-    }
-    h5 {
-      font-size: 0.75rem !important;
-    }
-    h6 {
-      font-size: 0.563rem !important;
-    }
-  }
+  ${artsbeacon}
 `
 
 const PureLayout = ({ children, data, customSEO }) => (
--- src/styles/index.js
+++ src/styles/index.js
@@ -1,5 +1,6 @@
 import prism from './prism'
 import theme from './theme'
 import reset from './reset'
+import artsbeacon from './artsbeacon'
 
-export { theme, reset, prism }
+export { theme, reset, prism, artsbeacon }
